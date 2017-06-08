905b4edd19b7f2e77c7119fe8545723078ed5914
qimeiyan <qmy20140422@163.com>
2017-06-08 20:14:44 +0800

    0608

--- src/containers/InvitationRewardlist.vue
+++ src/containers/InvitationRewardlist.vue
@@ -2,16 +2,15 @@
     <div flex="dir:top" flex-box="1" class="invitation-reward-list">
         <div class="header" flex-box="0">
             <div class="title">
-                <p class="right" @click.stop="$router.push('/reserve-detail?productReservationUuid='+item.productReservationUuid)">奖励细则</p>
-                <!-- 点击跳转到奖励细则页 -->
+                <p class="right" @click.stop="$router.push('/reward-detail')">奖励细则</p>
             </div>
             <ul flex="main:right">
                 <li flex-box="1">
-                    <p class='info'>{{unpaid | currencyFormat}}元</p>
+                    <p class='info'>{{sumData.unpaid | currencyFormat}}元</p>
                     <p class='tile'>待结算（税前）</p>
                 </li>
                 <li flex-box="1">
-                    <p class='info'>{{paidWithTax | currencyFormat}}元</p>
+                    <p class='info'>{{sumData.paidWithTax | currencyFormat}}元</p>
                     <p class='tile'>已结算（税后）</p>
                 </li> 
             </ul>
@@ -34,11 +33,11 @@
                             </li>
                             <li flex>
                                  <div flex-box="0">奖励比例：</div>
-                                 <div flex-box="0" class="width">{{item.rewardFactor}}%</div>
+                                 <div flex-box="0" class="width">{{item.rewardFactor}}</div>
                             </li>
                             <li flex class="last">
                                  <div flex-box="0">投资时间：</div>
-                                 <div flex-box="0">{{item.createTime}}</div>
+                                 <div flex-box="0">{{item.createTime|timeFormat}}</div>
                             </li>
                         </ul>
                         <span class="icon" :class='item.rewardStatus == 2 ? "finish" : "cancel"'>
@@ -60,49 +59,16 @@
             return{
                 unpaid:'',
                 paidWithTax:'',
-                // rewardList:[
-                //     {
-                //         investAmount: '10',
-                //         rewardFactor: '0.582',
-                //         createTime: '2014-03-22 18:00:00',
-                //         userMobile: '1871110000'
-                //     },
-                //     {
-                //         investAmount: '100',
-                //         rewardFactor: '0.582',
-                //         createTime: '2014-03-22 18:00:00',
-                //         userMobile: '1871110000'
-                //     },
-                //     {
-                //         investAmount: '100',
-                //         rewardFactor: '0.582',
-                //         createTime: '2014-03-22 18:00:00',
-                //         userMobile: '1871110000'
-                //     },
-                //     {
-                //         investAmount: '100',
-                //         rewardFactor: '0.582',
-                //         createTime: '2014-03-22 18:00:00',
-                //         userMobile: '1871110000'
-                //     },
-                //     {
-                //         investAmount: '10000',
-                //         rewardFactor: '0.582',
-                //         createTime: '2014-03-22 18:00:00',
-                //         userMobile: '1871110000'
-                //     },
-                // ]
+                sumData:{},
                 rewardList:[]
             }
         },
         created(){
-            let userUuid = this.$route.query.rewardType
             $api.get('/reward/list',{'rewardType':1,'pageNo':1,'pageSize':10})
                 .then(msg => {
-                    this.unpaid = msg.data.sumData.unpaid;
-                    this.paid = msg.data.sumData.paid;
+                    this.sumData = msg.data.sumData;
                     this.rewardList = msg.data.rewardList;
-                    console.log(msg)
+                    console.log(msg.data);
                     return msg
                 })
         },
