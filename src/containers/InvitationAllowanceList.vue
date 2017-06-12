82ca4c2da7fcc0d24fbf4e02cedd7195b7e1ed56
qimeiyan <qmy20140422@163.com>
2017-06-12 09:22:50 +0800

    code20170612 by qimeiyan

--- src/containers/InvitationAllowanceList.vue
+++ src/containers/InvitationAllowanceList.vue
@@ -2,7 +2,7 @@
     <div flex="dir:top" flex-box="1" class="invitation-allowance-list">
         <div class="header" flex-box="0">
             <div class="title">
-                <p class="right">说明</p>
+                <p class="right"  @click="show=true">说明</p>
             </div>
             <div class="tabs" flex='main:center'>
                 <div flex-box="1" class="tab" :class="{'tab-active':tab==1}" @click.stop="changeTab(1)">
@@ -46,6 +46,23 @@
                 </div>
             </div>
         </div>
+        <div flex="dir:top" flex-box="1" class="shade"  v-show="show">
+            <div class="cancel" flex='main:right' flex-box='1'  @click="show=false">关闭</div>
+            <div class="content"flex-box='4'>
+                <div>
+                    <p>您的直接理财师好友推荐客户购买平台产品后， 您将获得直接邀请津贴。</p>
+                    <p>奖励金额=好友奖励*奖励系数</p>
+                </div>
+                <div>
+                    <p>您的间接理财师好友推荐客户购买平台产品后， 您将获得间接邀请奖励。</p>
+                    <p>奖励金额=好友奖励*奖励系数</p>
+                </div>
+                <div>
+                    <p>注：直接邀请奖励系数为16%</p>
+                    <p>间接邀请奖励系数为4%</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -63,7 +80,8 @@
                 loading1: false,
                 loading2: false,
                 sumData: {},
-                tabList: []
+                tabList: [],
+                show:false
 
             }
         },
