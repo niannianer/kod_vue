6ecb0638ef67ee1b4e3fca5cf10bec38e8686dde
hetanhong <543039822@qq.com>
2017-06-14 11:00:26 +0800

    url

--- src/tools/config.js
+++ src/tools/config.js
@@ -6,5 +6,5 @@ export const testUrl = 'http://weixin-test.zj-hf.cn';
 export const productionUrl = 'https://xxx.production';
 export const telNumber = '400-8888-8888';
 export const baofooTestUrl = 'https://paytest.baofoo.com/baofoo-custody/custody/cerPayRecharge.do';
-export const nodeTestApi = 'http://59.110.217.183:8081/';
-export const nodeProductionApi = 'http://59.110.217.183:8081/';
+export const nodeTestApi = 'http://59.110.217.183:8081';
+export const nodeProductionApi = 'http://59.110.217.183:8081';
