47656b14f9605e099fbb0e2e55e58eba00faaeec
hetanhong <543039822@qq.com>
2017-07-05 18:22:49 +0800

    add back text

--- src/containers/HouseThree.vue
+++ src/containers/HouseThree.vue
@@ -406,6 +406,16 @@
         },
         created(){
             this.get();
+            requestHybrid({
+                tagname: 'title',
+                param: {
+                    backtype: 2,// "0 : 后退 1 : 直接关闭 2: 弹对话框",
+                    backAndRefresh: 1,
+                    title: '住房理财规划',
+                    backstr: '退出理财规划将不会保存，确认退出？',
+                    keyboard_mode: 0//0 adjustresize 1 adjustpan
+                }
+            });
         }
     }
 </script>
