7ad9d682f9d483980c867006030a4a682a62901b
wangwenbo <rruanruan@163.com>
2017-09-25 13:37:14 +0800

    edit fund

--- src/funds/HotList.vue
+++ src/funds/HotList.vue
@@ -1,6 +1,26 @@
 <template>
     <div class="hot-list">
-        hot-list
+        <div class="item">
+            <div class="title-part" flex="cross:center">
+                <p flex-box="0" class="tip">免费率</p>
+                <p flex-box="1">膨化添利宝货币市场基金</p>
+                <p flex-box="0">001666</p>
+            </div>
+            <div class="info-part" flex="box:mean">
+                <div class="info-box">
+                    <p class="orange f9">4.68%</p>
+                    <p class="info-title">七日年化率</p>
+                </div>
+                <div class="info-box">
+                    <p class="f9">0.01</p>
+                    <p class="info-title">起投金额</p>
+                </div>
+                <div class="info-box">
+                    <p class="f9">货币型</p>
+                    <p class="info-title">基金类型</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
