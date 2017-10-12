18ffc6b59609af25450184fb36a0c2e25d7f66c0
raoliqin <627974327@qq.com>
2017-10-12 09:24:29 +0800

    update

--- src/containers/Fund.vue
+++ src/containers/Fund.vue
@@ -1,49 +1,15 @@
 <template>
     <div class="funds">
-        <div class="banner">
-            <img :src="bannerDefault"/>
-            <div class="text">把握“基”会 精选基金任你选</div>
-        </div>
-        <div flex="box:mean main:justify cross:center" class="fund-link">
-            <div>
-                <img src="../images/fund/my-fund/fund-hot.png" class="img"/>
-                <div class="link-name">今日热销</div>
-            </div>
-            <div>
-                <img src="../images/fund/my-fund/fund-increase.png" class="img"/>
-                <div class="link-name">涨幅榜</div>
-            </div>
-            <div>
-                <img src="../images/fund/my-fund/fund-plan.png" class="img"/>
-                <div class="link-name">理财规划</div>
-            </div>
-        </div>
         <router-view></router-view>
-        <div class="footer">
-            <div class="server">
-                基金销售服务由<span class="link">盈米财富</span>提供
-            </div>
-            <div class="quali">
-                基金销售资格证号：000000378<span class="link">详情</span>
-            </div>
-        </div>
     </div>
 </template>
 
 <script>
     import '../less/funds.less'
-    import bannerDefault from '../images/fund/my-fund/fund-default.png';
     export default {
         name: 'base',
         data(){
             return {
-                bannerDefault,
-                noBanner: false,
-                bannerList: [],
-                newsList: [],
-                currentPage: 0,
-                pageSize: 3,
-                product: {}
             }
         },
         components: {},
--- src/funds/Lists.vue
+++ src/funds/Lists.vue
@@ -1,19 +1,47 @@
 <template>
     <div class="fund-lists">
-        <div class="header">
-            热销基金
+        <div class="banner">
+            <img :src="banner"/>
+            <div class="text">把握“基”会 精选基金任你选</div>
         </div>
-        <div class="fund-card" flex v-for="(item, index) in hotList" key="index">
-            <div class="card-left" flex-box="0">
-                <div class="number">{{item.rate|translatePate}}</div>
-                <div class="text">{{item.rateText}}</div>
+        <div flex="box:mean main:justify cross:center" class="fund-link">
+            <div>
+                <img src="../images/fund/my-fund/fund-hot.png" class="img"/>
+                <div class="link-name">今日热销</div>
             </div>
-            <div class="card-right"  flex-box="1" flex="dir:top">
-                <div flex-box="1" class="title">{{item.fundAbbrName}}<span class="num">{{item.fundCode}}</span></div>
-                <div flex-box="0">
-                    <span class="tag">反弹好</span>
-                    <span class="tag">回撤小</span>
+            <div>
+                <img src="../images/fund/my-fund/fund-increase.png" class="img"/>
+                <div class="link-name">涨幅榜</div>
+            </div>
+            <div>
+                <img src="../images/fund/my-fund/fund-plan.png" class="img"/>
+                <div class="link-name">理财规划</div>
+            </div>
+        </div>
+        <div class="list">
+            <div class="header">
+                热销基金
+            </div>
+            <div class="fund-card" flex v-for="(item, index) in hotList" key="index">
+                <div class="card-left" flex-box="0">
+                    <div class="number">{{item.rate|translatePate}}</div>
+                    <div class="text">{{item.rateText}}</div>
                 </div>
+                <div class="card-right"  flex-box="1" flex="dir:top">
+                    <div flex-box="1" class="title">{{item.fundAbbrName}}<span class="num">{{item.fundCode}}</span></div>
+                    <div flex-box="0">
+                        <span class="tag">反弹好</span>
+                        <span class="tag">回撤小</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="server">
+                基金销售服务由<span class="link">盈米财富</span>提供
+            </div>
+            <div class="quali">
+                基金销售资格证号：000000378<span class="link">详情</span>
             </div>
         </div>
     </div>
@@ -21,12 +49,14 @@
 
 <script>
     import $api from '../tools/api';
-    import '../less/funds.less'
+    import '../less/funds.less';
+    import banner from '../images/fund/my-fund/fund-default.png';
     export default {
         name: 'base',
         data(){
             return {
-                hotList: []
+                hotList: [],
+                banner
             }
         },
         components: {},
@@ -36,9 +66,12 @@
         computed: {},
         methods: {
             hostList(){
-                $api.get('/fund/info/host/list').then((resp) => {
+                $api.get('/fund/info/host/list', {
+                    startRow: 0,
+                    pageSize: 3
+                }).then((resp) => {
                     if (resp.code == 200) {
-                        this.hotList = resp.data.list.slice(0, 3);
+                        this.hotList = resp.data.list;
                         this.hotList.map((v)=>{
                             if(v.fundType == 4){
                                 v.rate = v.yearlyRoe;
