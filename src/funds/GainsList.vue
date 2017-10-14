b5ef23e3e613a47cffb7b2380b9cbad302ee8cdb
wangwenbo <rruanruan@163.com>
2017-10-14 15:56:00 +0800

    edit fund

--- src/funds/GainsList.vue
+++ src/funds/GainsList.vue
@@ -23,73 +23,90 @@
             </div>
         </div>
         <div class="body" flex>
-            <div class="title-list" flex-box="0" v-for="(item,index) in list" :key="index">
+            <div flex-box="0" class="title-list">
                 <div class="bg-content fund-name" flex-box="0">
-                    <div class="title-item" v-if="index == 0">
+                    <div class="title-item">
                         基金名称
                     </div>
-                    <div class="item-info" flex="cross:center main:center" >
-                        循环元素1
+                    <div class="item-info" flex="cross:center main:center" v-for="(item,index) in list">
+                        {{item.fundAbbrName}}
                     </div><!--内容溢出怎么办？-->
                 </div>
-
             </div>
-            <div>
-                <div class="overflow" flex>
-                    <div class="item-wrap bg-grey" flex-box="0">
-                        <div class="title-item">
-                            单位净值
+            <div class="title-list" flex-box="1" flex style="overflow: auto">
+                <div>
+                    <div  flex>
+                        <div class="item-wrap bg-grey" flex-box="0">
+                            <div class="title-item">
+                                单位净值
+                            </div>
                         </div>
-                        <div class="item-info" flex="cross:center main:center">
-                            循环元素1
+                        <div class="item-wrap bg-content" flex-box="0">
+                            <div class="title-item">
+                                日涨幅
+                            </div>
                         </div>
-                    </div>
-                    <div class="item-wrap bg-content" flex-box="0">
-                        <div class="title-item">
-                            日涨幅
+                        <div class="item-wrap bg-grey" flex-box="0">
+                            <div class="title-item">
+                                周涨幅
+                            </div>
                         </div>
-                        <div class="item-info red f8" flex="cross:center main:center">
-                            循环元素1
+                        <div class="item-wrap bg-content" flex-box="0">
+                            <div class="title-item">
+                                月涨幅
+                            </div>
                         </div>
-                    </div>
-                    <div class="item-wrap bg-grey" flex-box="0">
-                        <div class="title-item">
-                            周涨幅
+                        <div class="item-wrap bg-grey" flex-box="0">
+                            <div class="title-item">
+                                季涨幅
+                            </div>
                         </div>
-                        <div class="item-info red f8" flex="cross:center main:center">
-                            循环元素1
+                        <div class="item-wrap bg-content" flex-box="0">
+                            <div class="title-item">
+                                近一年涨幅
+                            </div>
                         </div>
-                    </div>
-                    <div class="item-wrap bg-content" flex-box="0">
-                        <div class="title-item">
-                            月涨幅
-                        </div>
-                        <div class="item-info" flex="cross:center main:center">
-                            循环元素1
-                        </div>
-                    </div>
-                    <div class="item-wrap bg-grey" flex-box="0">
-                        <div class="title-item">
-                            季涨幅
-                        </div>
-                        <div class="item-info" flex="cross:center main:center">
-                            循环元素1
+                        <div class="item-wrap bg-grey" flex-box="0">
+                            <div class="title-item">
+                                今年涨幅
+                            </div>
                         </div>
                     </div>
-                    <div class="item-wrap bg-content" flex-box="0">
-                        <div class="title-item">
-                            近一年涨幅
+                    <div  flex v-for="(item,index) in list">
+                        <div class="item-wrap bg-grey" flex-box="0">
+                            <div class="item-info" flex="cross:center main:center">
+                                循环元素1
+                            </div>
                         </div>
-                        <div class="item-info" flex="cross:center main:center">
-                            循环元素1
+                        <div class="item-wrap bg-content" flex-box="0">
+                            <div class="item-info red f8" flex="cross:center main:center">
+                                循环元素1
+                            </div>
                         </div>
-                    </div>
-                    <div class="item-wrap bg-grey" flex-box="0">
-                        <div class="title-item">
-                            今年涨幅
+                        <div class="item-wrap bg-grey" flex-box="0">
+                            <div class="item-info red f8" flex="cross:center main:center">
+                                循环元素1
+                            </div>
                         </div>
-                        <div class="item-info" flex="cross:center main:center">
-                            循环元素1
+                        <div class="item-wrap bg-content" flex-box="0">
+                            <div class="item-info" flex="cross:center main:center">
+                                循环元素1
+                            </div>
+                        </div>
+                        <div class="item-wrap bg-grey" flex-box="0">
+                            <div class="item-info" flex="cross:center main:center">
+                                循环元素1
+                            </div>
+                        </div>
+                        <div class="item-wrap bg-content" flex-box="0">
+                            <div class="item-info" flex="cross:center main:center">
+                                循环元素1
+                            </div>
+                        </div>
+                        <div class="item-wrap bg-grey" flex-box="0">
+                            <div class="item-info" flex="cross:center main:center">
+                                循环元素1
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -115,16 +132,16 @@
                 currentPage: 0,
                 pageSize: 10,
                 loading: true,
-                orderBy:'nav'
+                orderBy: 'nav'
             }
         },
         created(){
-            $api.get('/fund/info/increase/list',{
+            $api.get('/fund/info/increase/list', {
                 startRow: this.currentPage * this.pageSize,
                 pageSize: this.pageSize,
-                fundType:this.fundType,//0其他,//1股票型,//2债券型,//3混合型,//4货币型,//5保本型,//6指数型,//7QDII,//8商品型,//9短期理财,256全部
-                sort:'ASC',///DESC
-                orderBy:this.orderBy
+                fundType: this.fundType,//0其他,//1股票型,//2债券型,//3混合型,//4货币型,//5保本型,//6指数型,//7QDII,//8商品型,//9短期理财,256全部
+                sort: 'ASC',///DESC
+                orderBy: this.orderBy
             })
                 .then(resp => {
                     this.list = resp.data.list
