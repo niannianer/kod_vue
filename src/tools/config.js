b123ec27bd6716f264792b00cff4a6c218ce1e60
hetanhong <543039822@qq.com>
2017-10-14 14:59:25 +0800

    step

--- src/tools/config.js
+++ src/tools/config.js
@@ -13,7 +13,7 @@ let apiNode = `${protocol}//market-test.zj-hf.cn`;
 let merchant_id = '100000675';
 let terminal_id = '100000701';
 /*test env*/
-if (process.env.kingold == 'test') {
+if (process.env.kingold == 'test'||1) {
     apiUrl = `${protocol}//weixin-test.zj-hf.cn`;
     baofooUrl = 'https://paytest.baofoo.com/baofoo-custody/custody/';
     baofooCallUrl = `${protocol}//notify-test.zj-hf.cn`;
