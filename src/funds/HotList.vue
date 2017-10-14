36c18f13ad8377524a3f8f9aa30e0dec47ed7512
wangwenbo <rruanruan@163.com>
2017-10-14 15:04:24 +0800

    edit fund

--- src/funds/HotList.vue
+++ src/funds/HotList.vue
@@ -1,42 +1,98 @@
 <template>
     <div class="hot-list">
-        <div class="item">
-            <div class="float left"></div>
-            <div class="float right"></div>
-            <div class="title-part" flex="cross:center">
-                <p flex-box="0" class="tip">免费率</p>
-                <p flex-box="1">膨化添利宝货币市场基金</p>
-                <p flex-box="0">001666</p>
-            </div>
-            <div class="info-part" flex="box:mean">
-                <div class="info-box">
-                    <p class="orange f9">4.68%</p>
-                    <p class="info-title">七日年化率</p>
-                </div>
-                <div class="info-box">
-                    <p class="f9">0.01</p>
-                    <p class="info-title">起投金额</p>
-                </div>
-                <div class="info-box">
-                    <p class="f9">货币型</p>
-                    <p class="info-title">基金类型</p>
-                </div>
-            </div>
-        </div>
+        <mt-loadmore :top-method="loadTop" ref="loadmore" :auto-fill="autoFill">
+            <ul v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="10">
+                <li class="item" v-for="(item,index) in lists">
+                    <div class="float left"></div>
+                    <div class="float right"></div>
+                    <div class="title-part" flex="cross:center">
+                       <!-- <p flex-box="0" class="tip">免费率</p>-->
+                        <p flex-box="1">{{item.fundFullName}}</p>
+                        <p flex-box="0">{{item.fundCode}}</p>
+                    </div>
+                    <div class="info-part" flex="box:mean">
+                        <div class="info-box" v-if="Number(item.fundType)==4">
+                            <p class="orange f9">{{item.yearlyRoe}}</p>
+                            <p class="info-title">七日年化率</p>
+                        </div>
+                        <div class="info-box" v-if="Number(item.fundType)!=4&&item.oneYearReturn">
+                            <p class="orange f9">{{item.oneYearReturn}}</p>
+                            <p class="info-title">近一年涨幅</p>
+                        </div>
+                        <div class="info-box" v-if="Number(item.fundType)!=4&&!item.oneYearReturn">
+                            <p class="orange f9 min-height">{{item.thisYearReturn}}</p>
+                            <p class="info-title">今年以来涨幅</p>
+                        </div>
+                        <div class="info-box">
+                            <p class="f9 min-height">{{item.minAmtIndiFirstPurch|currencyFormat}}</p>
+                            <p class="info-title">起投金额</p>
+                        </div>
+                        <div class="info-box">
+                            <p class="f9">{{fundType[Number(item.fundType)]}}</p>
+                            <p class="info-title">基金类型</p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </mt-loadmore>
+
     </div>
 </template>
 
 <script>
+    import $api from '../tools/api';
+    import Vue from 'vue';
+    import {Loadmore, InfiniteScroll} from 'mint-ui';
+    Vue.component(Loadmore.name, Loadmore);
+    Vue.use(InfiniteScroll);
     import '../less/fund/hot-list.less';
     export default {
         name: 'hot-list',
         data(){
-            return {}
+            return {
+                lists: [],
+                autoFill: false,
+                currentPage: 0,
+                pageSize: 10,
+                loading: true,
+                fundType: ['其他类型', '股票型', '债券型', '混合型', '货币型', '保本型', '指数型', 'QDII', '商品型', '短期理财'],
+            }
         },
         created(){
+            this.loadData();
         },
         computed: {},
-        methods: {},
+        methods: {
+            loadTop(){
+                this.lists = [];
+                this.currentPage = 0;
+                this.loadData().then(() => {
+                    this.$refs.loadmore.onTopLoaded();
+                });
+            },
+            loadData(){
+                return $api.get('/fund/info/host/list', {
+                    startRow: this.currentPage * this.pageSize,
+                    pageSize: this.pageSize
+                })
+                    .then(msg => {
+                        if (msg.code == 200) {
+                            this.lists = this.lists.concat(msg.data.list);
+                            if (msg.data.list.length < this.pageSize) {
+                                this.loading = true;
+                            } else {
+                                this.loading = false;
+                            }
+                        }
+                        return msg
+                    })
+            },
+            loadMore(){
+                this.loading = true;
+                this.currentPage++;
+                this.loadData();
+            }
+        },
         destroyed(){
 
         }
