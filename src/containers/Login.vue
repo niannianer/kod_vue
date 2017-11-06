53bf792a9cc48d4df6bf5de28b815fa89301dfe6
wangwenbo <rruanruan@163.com>
2017-11-06 10:27:49 +0800

    edit login

--- src/containers/Login.vue
+++ src/containers/Login.vue
@@ -1,59 +1,68 @@
 <template>
-    <div class="login" ref="login">
+    <!--<div class="login" ref="login">
         <div class="logo-warp" >
             <img class="logo" src="../images/logo.png"/>
         </div>
         <div class="title">金疙瘩</div>
 
-        <div class="form">
-            <label for="username">
-                <div class="input-warp" flex>
-                    <img flex-box="0" class="phone" src="../images/login/phone_icon1@2x.png"/>
-                    <input type="tel" flex-box="1"
-                           v-model.trim="username"
-                           placeholder="请输入手机号"
-                           class="input" id="username"/>
-                    <div   flex="main:right cross:center" >
-                        <img  class="input-img" src="../images/login/pwd-show@2x.png"/>
-                    </div>
-                </div>
-            </label>
-            <label for="password">
-                <div class="input-warp" flex>
-                    <img flex-box="0" class="phone" src="../images/login/key_icon@2x.png"/>
-                    <input v-if="showPassword" type="text" class="input" flex-box="1" id="password"
-                           placeholder="请输入密码"
-                           v-model.trim="password"/>
-                    <input v-else type="password" class="input"
-                           placeholder="请输入密码"
-                           flex-box="1" id="password1" v-model.trim="password"/>
-                    <div class="pass" flex="main:right cross:center" @click.stop="showPassword=!showPassword">
-                        <img class="show-hide" v-if="showPassword" src="../images/login/pwd-show@2x.png"/>
-                        <img class="show-hide" src="../images/login/pwd-hide@2x.png" v-else/>
-                    </div>
 
-                </div>
-            </label>
-            <label for="image-code" class="label-warp" flex v-if="imageCode">
-                <div class="code-warp" flex-box="1">
-                    <input id="image-code" class="input"
-                           v-model.trim="inputCode"
-                           placeholder="请输入图形校验码"/>
-                </div>
-                <div class="image-warp" flex-box="0">
-                    {{imageCode}}
-                </div>
-            </label>
-        </div>
 
         <div class="btn-warp">
             <button class="btn-primary btn-login" @click.stop="login">登  录</button>
         </div>
-        <!--footer -->
+        &lt;!&ndash;footer &ndash;&gt;
       <div class="footer" flex>
           <span class="link" flex-box="1" @click.stop="pathTo('/register')">立即注册</span>
           <router-link class="link" flex-box="1" :to="{path:'/find-password'}" replace>忘记密码</router-link>
       </div>
+    </div>-->
+    <div class="login">
+        <div class="header" flex="box:mean">
+            <p>密码登录</p>
+            <p>验证码登录</p>
+        </div>
+        <div class="body">
+            <div class="form">
+                <label for="username">
+                    <div class="input-warp" flex="cross:center">
+                        <img flex-box="0" class="phone" src="../images/login/phone_icon1@2x.png"/>
+                        <input type="tel" flex-box="1"
+                               v-model.trim="username"
+                               placeholder="请输入手机号码"
+                               class="input" id="username"/>
+                        <div   flex="main:right cross:center" >
+                            <img  class="input-img" src="../images/login/pwd-show@2x.png"/>
+                        </div>
+                    </div>
+                </label>
+                <label for="password">
+                    <div class="input-warp" flex="cross:center">
+                        <img flex-box="0" class="phone" src="../images/login/key_icon@2x.png"/>
+                        <input v-if="showPassword" type="text" class="input" flex-box="1" id="password"
+                               placeholder="请输入密码"
+                               v-model.trim="password"/>
+                        <input v-else type="password" class="input"
+                               placeholder="请输入密码"
+                               flex-box="1" id="password1" v-model.trim="password"/>
+                        <div class="pass" flex="main:right cross:center" @click.stop="showPassword=!showPassword">
+                            <img class="show-hide" v-if="showPassword" src="../images/login/pwd-show@2x.png"/>
+                            <img class="show-hide" src="../images/login/pwd-hide@2x.png" v-else/>
+                        </div>
+
+                    </div>
+                </label>
+                <label for="image-code" class="label-warp" flex v-if="imageCode">
+                    <div class="code-warp" flex-box="1">
+                        <input id="image-code" class="input"
+                               v-model.trim="inputCode"
+                               placeholder="请输入图形校验码"/>
+                    </div>
+                    <div class="image-warp" flex-box="0">
+                        {{imageCode}}
+                    </div>
+                </label>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -143,9 +152,9 @@
                             window.sessionStorage.removeItem('logoutUrl');
                             window.sessionStorage.removeItem('logoutIndex');
                             logoutUrl = decodeURIComponent(logoutUrl);
-                            if(logoutUrl&&/http/.test(logoutUrl)){
+                            if (logoutUrl && /http/.test(logoutUrl)) {
                                 window.location.replace(logoutUrl);
-                            }else {
+                            } else {
 
                                 this.$store.dispatch('getAccountBaofoo');
                                 this.$store.dispatch('getBankInfo');
@@ -154,7 +163,7 @@
 
                             }
                             return false;
-                        }else {
+                        } else {
                             Toast(data.msg);
                         }
 
@@ -163,7 +172,7 @@
 
             },
             pathTo(path){
-                if(path == '/register'){
+                if (path == '/register') {
                     let event = ['_trackEvent', '注册', 'SHOW', '从登录页进入注册页', '从登录页进入注册页'];
                     window._hmt.push(event);
                 }
@@ -173,7 +182,7 @@
         },
         mounted(){
             let heigth = window.innerHeight;
-            this.$refs.login.style.height = heigth+'px';
+            //  this.$refs.login.style.height = heigth+'px';
         }
     }
 </script>
