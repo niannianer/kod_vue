b0a8693c37477f12de1a8c38ad2288b88c07f229
hetanhong <543039822@qq.com>
2018-02-28 09:58:04 +0800

    dd

--- src/containers/PersonalCenter.vue
+++ src/containers/PersonalCenter.vue
@@ -291,8 +291,8 @@
                 'investorType'
             ]),
             masterList(){
-                // 已开启特权
-                if (this.investorType >= 12) {
+                // 投资好友已到10人
+                if (this.relationInvest >= 10) {
                     return ['理财达人，长期奖励', '额外奖励   收益加速'];
                 }
                 return ['理财达人，长期奖励', `还需${10 - this.relationInvest}个投资好友`, '额外奖励   收益加速'];
